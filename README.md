a2cb268b8a38a20ef7c3fffb2d3b56ebefff59e3
John Solarz <johnsolarz@gmail.com>
2013-09-12 02:49:15 -0500

    1.0

--- README.md
+++ README.md
@@ -1,15 +1,7 @@
-WP Dashboard
-============
+Dashboard
+=========
 
-Eight Seven Central WordPress login, dashboard and widgets
-
-Install
--------
-
-1. Add the include in functions.php to your theme's functions.php file
-2. Add dashboard.php, login.css, login.png to their respective directories
-3. Adjust dependency URL's in functions.php, dashboard.php and login.css as needed
-4. Look sharp
+Custom WordPress login page, dashboard and sidebar widgets. Copyright (C) Eight Seven Central.
 
 Author
 ------
--- dashboard.php
+++ dashboard.php
@@ -1,12 +1,40 @@
 <?php
+/*
+Plugin Name: Dashboard
+Plugin URI:  http://eightsevencentral.com
+Description: Custom login page, dashboard and sidebar widgets.
+Version:     1.0
+Author:      Eight Seven Central
+Author URI:  http://eightsevencentral.com
+License:     GPL2
+*/
+
+/*
+
+    Copyright (C) 2013  Eight Seven Central  (email : design@eightsevencentral.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 /**
- * Login screen css
+ * Load login.css on login page.
  */
 function esc_login_css() {
-  // adjust to match your theme structure
-  echo '<link rel="stylesheet" href="' . get_template_directory_uri() . '/assets/css/login.css">';
+  wp_register_style( 'esc_login', plugins_url( 'css/login.css', __FILE__ ), array(), '2013', 'all' );
+  wp_enqueue_style( 'esc_login' );
 }
-add_action('login_head', 'esc_login_css');
+add_action( 'login_enqueue_scripts', 'esc_login_css', 10 );
 
 /**
  * Login URL and title
@@ -24,7 +52,7 @@ function esc_login_title() {
 add_filter( 'login_headertitle', 'esc_login_title', 10, 4 );
 
 /**
- * Dashboard widget
+ * Add ESC News widget
  */
 add_action('wp_dashboard_setup', 'esc_add_dashboard_widgets');
 function esc_add_dashboard_widgets() {
@@ -44,11 +72,29 @@ function dashboard_esc_feed_output() {
 }
 
 /**
+ * Remove default WP widgets
+ */
+function esc_dashboard_widgets() {
+  global $wp_meta_boxes;
+
+  //unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);
+  //unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);
+  //unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']);
+  unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);
+  unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_recent_drafts']);
+  //unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']);
+  unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);
+  unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
+
+}
+add_action('wp_dashboard_setup', 'esc_dashboard_widgets' );
+
+/**
  * Footer credits
  */
 add_filter( 'admin_footer_text', 'esc_admin_footer_text' );
 function esc_admin_footer_text( $default_text ) {
-  return '<span id="footer-thankyou">Design + Development by <a href="http://eightsevencentral.com">Eight Seven Central</a><span> &mdash; Powered by <a href="http://www.wordpress.org">WordPress</a>';
+  return '<span id="footer-thankyou">Design + Development by <a href="http://eightsevencentral.com">Eight Seven Central</a>';
 }
 
 /**
@@ -73,7 +119,7 @@ add_action( 'admin_init', create_function('', 'remove_action( \'admin_notices\',
  *
  * @link http://www.everparent.com/lunaticfred/2011/05/05/how-to-remove-default-sidebar-widgets-in-wordpress/
  */
-function esc_default_widgets() {
+function esc_sidebar_widgets() {
   //unregister_widget('WP_Widget_Pages');
   unregister_widget('WP_Widget_Calendar');
   unregister_widget('WP_Widget_Archives');
@@ -87,4 +133,4 @@ function esc_default_widgets() {
   unregister_widget('WP_Widget_Tag_Cloud');
   //unregister_widget('WP_Nav_Menu_Widget');
 }
-add_action( 'widgets_init', 'esc_default_widgets' );
+add_action( 'widgets_init', 'esc_sidebar_widgets' );
